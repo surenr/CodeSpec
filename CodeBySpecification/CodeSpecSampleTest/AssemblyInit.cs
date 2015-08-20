5c1c90eb0d88318858a5cb5a72768ae769ffd7f6
Milindu Sanoj Kumarage <milinduk@99x.lk>
2015-08-20 13:46:53 +0530

    IFrame tests added

---
+++ CodeBySpecification/CodeSpecSampleTest/AssemblyInit.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeBySpecification.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeSpecSampleTest
+{
+	internal class AssemblyInit
+	{
+		[AssemblyInitialize]
+		public void Initialize()
+		{
+			Trace.WriteLine("Initializing Feature Base");
+			var baseFeature = new FeatureBase();
+			Trace.WriteLine(string.Format("Instantiated {0}", baseFeature));
+		}
+	}
+}
